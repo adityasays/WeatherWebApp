60149a632108401e7b7b52472fd591f601fe5bd2
adityasays <aditya.shukla019r@gmail.com>
2023-09-03 15:16:12 +0530

    structural changes and logic updates

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 const cursor = document.getElementsByClassName("cursor")[0];
-let timeout; 
+const search = document.getElementsByClassName("citysearch")[0];
+const searchbtn = document.getElementsByClassName("searchbtn")[0];
+let timeout;
 
 document.addEventListener("mousemove", (e) => {
   let x = e.pageX;
@@ -7,34 +9,52 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.top = y + "px";
   cursor.style.left = x + "px";
 
- 
   clearTimeout(timeout);
-  cursor.style.display = "block"; 
+  cursor.style.display = "block";
   timeout = setTimeout(mousestop, 1000);
 });
 
 function mousestop() {
   cursor.style.display = "none";
 }
-
+const weathericon = document.getElementsByClassName("weaicon")[0]
 const apikey = "d7bf674cb42263f8a888cff7bec2f45c";
-const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=lucknow";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkweather() {
+async function checkweather(city) {
   try {
-    const response = await fetch(apiurl + `&appid=${apikey}`);
+    const response = await fetch(apiurl + city + `&appid=${apikey}`);
     if (!response.ok) {
-      throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     console.log(data);
     document.getElementsByClassName("city")[0].innerHTML = data.name;
-    document.getElementsByClassName("temp")[0].innerHTML = Math.round(data.main.temp) +"°C";
-    document.getElementsByClassName("humi")[0].innerHTML = data.main.humidity +"%";
-    document.getElementsByClassName("wind")[0].innerHTML = data.wind.speed +" Km/hr";
-  } catch (error) {
-    console.error('An error occurred:', error);
+    document.getElementsByClassName("temp")[0].innerHTML = Math.round(data.main.temp) + "°C";
+    document.getElementsByClassName("humi")[0].innerHTML = data.main.humidity + "%";
+    document.getElementsByClassName("wind")[0].innerHTML = data.wind.speed + " Km/hr";
+    if(data.weather[0].main =="Clouds")
+    {
+     weathericon.innerHTML=`<i class="fa-solid fa-cloud fa-xl" style="color: #ffffff;"></i>`
+    }
+    else if(data.weather[0].main=="Rain"){
+      weathericon.innerHTML=`<i class="fa-solid fa-cloud-rain fa-xl" style="color: #ffffff;"></i>`
+  } 
+  else if(data.weather[0].main=="Drizzle"){
+    weathericon.innerHTML=`<i class="fa-solid fa-cloud-sun-rain fa-xl" style="color: #ffffff;"></i>`
+} 
+else if(data.weather[0].main=="Mist"){
+  weathericon.innerHTML=`<i class="fa-solid fa-smog fa-xl" style="color: #ffffff;"></i>`
+} 
+else if(data.weather[0].main=="Snow"){
+  weathericon.innerHTML=`<i class="fa-solid fa-snowflake fa-xl" style="color: #ffffff;"></i>`
+} }
+  catch (error) {
+    console.error("An error occurred:", error);
   }
 }
 
-checkweather();
+
+searchbtn.addEventListener("click", () => {
+  checkweather(search.value);
+});
